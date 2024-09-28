1581182389fbb8b00d9371e786784f924401e1c8
Swathi-18502 <swathiderangula2002@gmail.com>
2024-09-28 14:58:14 +0530

    today demo class

---
+++ src/main/java/com/neoteric/java/App.java
@@ -0,0 +1,20 @@
+package com.neoteric.java;
+
+import java.util.Date;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        Human human= new Human();
+        human.name="neo";
+        human.age=21;
+        human.dob=new Date();
+
+    }
+
+}
---
+++ src/main/java/com/neoteric/java/Human.java
@@ -0,0 +1,11 @@
+package com.neoteric.java;
+
+import java.util.Date;
+
+public class Human {
+    String name;
+    int age;
+    Date dob;
+    String gender;
+
+}
